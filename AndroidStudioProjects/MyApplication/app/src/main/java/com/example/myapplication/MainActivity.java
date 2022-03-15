78c34bb194b5d147355d9aeb907a69d123166c72
ANTi1302 <porkoe3443334@gmail.com>
2022-03-15 17:49:51 +0700

    demo

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.ListView;
 
 import com.example.myapplication.adopet.Adopet;
 import com.example.myapplication.model.Donut;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +21,13 @@ import static com.example.myapplication.R.id.itemLayout;
 public class MainActivity extends AppCompatActivity {
 
     private List<Donut> donutList;
+    private List<Donut> donutListSearch;
     private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        donutListSearch= new ArrayList<>();
         listView= (ListView) findViewById(R.id.listView);
         listView.setDivider(null);
         donutList= new ArrayList<>();
@@ -38,6 +41,32 @@ public class MainActivity extends AppCompatActivity {
         Adopet adopet= new Adopet(this,R.layout.item_layout, donutList);
         listView.setAdapter(adopet);
 
+        TextInputEditText textInputEditText= findViewById(R.id.timKiem);
+        TextInputLayout textInputLayout= findViewById(R.id.btnTimKiem);
 
-    }
+
+
+    textInputLayout.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int count=0;
+            for (Donut donut : donutList) {
+                String temp = textInputEditText.getText().toString();
+                if (donut.getTenDonut().toString().contains(temp)) {
+                    donutListSearch.add(donut);
+
+                }
+
+            }
+            listView.setAdapter(new Adopet(MainActivity.this, R.layout.item_layout, donutListSearch));
+
+
+
+
+        }
+    });
+
+}
+
+
 }
